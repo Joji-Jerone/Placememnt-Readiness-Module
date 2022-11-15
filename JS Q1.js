f4f5d2b5440dc072146b125a9d12481d772e0987
Joji-Jerone <100847982+Joji-Jerone@users.noreply.github.com>
2022-11-15 22:31:53 +0530

    Create JS Q1.js

---
+++ JS Q1.js
@@ -0,0 +1,13 @@
+let num = 1234;
+    
+    function reverseDigits(num) {
+        let rev_num = 0;
+        while(num > 0)
+        {
+            rev_num = rev_num * 10 + num % 10;
+            num = Math.floor(num / 10);
+        }
+        return rev_num;
+    }
+ 
+    document.write(reverseDigits(num));
